f30dfb80b39d8adb4db9a2473f4af5c04f253fb7
Apoorv Kansal <apoorvkansalak@gmail.com>
2022-03-03 23:51:44 +0530

    Added route to edit team

--- server/routers/teams/handler.ts
+++ server/routers/teams/handler.ts
@@ -1,7 +1,7 @@
 import { Response } from "express";
 import { INCOMPLETE_DATA, INTERNAL_SERVER_ERROR } from "./../../errors";
 import { AuthenticatedTypedRequest } from "../../types";
-import { Prisma } from "@prisma/client";
+import { Prisma, Team } from "@prisma/client";
 import prisma from "../../prisma";
 
 export const createTeam = async (
@@ -96,6 +96,55 @@ export const getTeam = async (
     where: {
       id: parseInt(req.params.id, 10),
     },
+    include: {
+      users: true,
+      phoneNumber: true,
+    },
   });
   return res.json(team);
+};
+
+export const updateTeam = async (
+  req: AuthenticatedTypedRequest<
+    {
+      name: string;
+      phoneNumberId: null | number;
+      userIds: number[];
+      type: Team["callType"];
+    },
+    {},
+    { id: string }
+  >,
+  res: Response
+) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+    const { name, type, phoneNumberId, userIds } = req.body;
+
+    const team = await prisma.team.update({
+      where: {
+        id,
+      },
+      data: {
+        name,
+        callType: type,
+        users: {
+          set: userIds.map((id) => ({
+            id,
+          })),
+        },
+        phoneNumber: phoneNumberId
+          ? {
+              connect: { id: phoneNumberId },
+            }
+          : {
+              disconnect: true,
+            },
+      },
+    });
+    return res.json(team);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(INTERNAL_SERVER_ERROR);
+  }
 };
--- server/routers/teams/index.ts
+++ server/routers/teams/index.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import { isAdmin } from "../../utils/auth";
-import { createTeam, deleteTeam, getTeam, getTeams } from "./handler";
+import {
+  createTeam,
+  deleteTeam,
+  getTeam,
+  getTeams,
+  updateTeam,
+} from "./handler";
 
 // Handles all /api/teams routes
 
@@ -9,6 +15,7 @@ const router = express.Router();
 router.post("/", isAdmin, createTeam);
 router.post("/delete", isAdmin, deleteTeam);
 router.get("/:id", isAdmin, getTeam);
+router.post("/:id", isAdmin, updateTeam);
 router.get("/", isAdmin, getTeams);
 
 export default router;
